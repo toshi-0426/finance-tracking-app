01d1af7ba224f7c0dfac0ba3ed8cdcb7f2308807
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-05-30 22:41:19 +0900

    footer done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import PageHeader from "@/components/page-header";
 import { Metadata } from "next";
 import { ReactNode } from "react";
@@ -18,9 +19,9 @@ export default function Layout({ children }: { children: ReactNode }){
             <main>
                 {children}
             </main>
-            <footer className="mt-auto text-center pb-8">
-                Footer
-            </footer>
+            <div className="mt-auto mb-5 text-center pb-4 flex justify-center pt-16">
+                <Footer />
+            </div>
         </>
     )
 }
---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+
+import { GitHub } from "@mui/icons-material";
+import Link from "next/link";
+
+
+export default function Footer() {
+    return <Link 
+            href="https://github.com/toshi-0426/finance-tracking-app" 
+            className="flex items-center space-x-2 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+                <GitHub />
+                <span>GitHub</span>
+          </Link>
+}
